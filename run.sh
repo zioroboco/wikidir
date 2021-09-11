92f66621b53446f8e52b365440fd5411c5370634
Louis Quinnell <_@ziorobo.co>
2021-09-11 23:43:17 +1000

    Basic repo parsing and wiki cloning

--- run.sh
+++ run.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
 set -e
 
-echo "Hello from wiki-as-docs-dir!"
+if [ -d docs ]; then
+  exit 0
+fi
+
+repo=$(node ./get-repo)
+wiki="ssh://git@github.com/$repo.wiki.git"
+
+echo "Cloning $wiki..."
+git clone "$wiki" ./docs
+
+echo "# See [wiki](https://github.com/$repo/wiki) for docs..." >> docs/README.md
+git add docs/README.md
+
+echo "/.gitignore" >> docs/.gitignore
+echo "/README.md" >> docs/.gitignore
+git add -f docs/.gitignore
+
+echo "/docs/" >> .gitignore
+git add .gitignore

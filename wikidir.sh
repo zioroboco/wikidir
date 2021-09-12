3ad7c624a5358e4bf2a8735517de6fd1e52d302a
Louis Quinnell <_@ziorobo.co>
2021-09-12 19:16:26 +1000

    Get wiki url from git remotes
    Closes #1

--- wikidir.sh
+++ wikidir.sh
@@ -7,13 +7,28 @@ init() {
     exit 1
   fi
 
-  repo=$(node "$(dirname "$0")"/../wikidir/get-repo)
-  wiki="https://github.com/$repo.wiki.git"
+  # e.g. origin/master
+  remote_ref=$(git rev-parse --abbrev-ref --symbolic-full-name @\{u\})
 
-  echo "Cloning $wiki..."
-  git clone "$wiki" ./docs
+  # e.g. origin
+  remote_name=$(echo "$remote_ref" | cut -d'/' -f1)
 
-  echo "### [Docs are in the wiki.](https://github.com/$repo/wiki)" >> README.md.tmp
+  # e.g. git@github.com:zioroboco/wikidir.git
+  remote_url=$(git remote get-url "$remote_name")
+
+  # e.g. zioroboco/wikidir
+  repo_slug=$(echo "$remote_url" | sed 's/\.git$//' | sed 's/.*github.com//' | sed 's/://')
+
+  # e.g. https://github.com/zioroboco/wikidir
+  base_url="https://github.com/$repo_slug"
+
+  # e.g. https://github.com/zioroboco/wikidir.wiki.git
+  wiki_url="$base_url.wiki.git"
+
+  echo "Cloning $wiki_url..."
+  git clone "$wiki_url" ./docs
+
+  echo "### [Docs are in the wiki.]($base_url/wiki)" >> README.md.tmp
   git add README.md.tmp
   git mv README.md.tmp docs/README.md
 
